0fa132e7997cd27ad99a63f6ee3e6030f3ef6889
Otar Murmanishvili <murmanishvili94@gmail.com>
2024-03-06 12:44:31 +0400

    react default update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App() {
